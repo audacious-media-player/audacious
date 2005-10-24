67ac2f706563a4cfa880ebc4a8298a4541c33b32
nenolod <devnull@localhost>
2005-10-24 16:57:20 -0700

    [svn] Regenerate Makefiles, AAC fixes.

--- Input/aac/include/systems.h
+++ Input/aac/include/systems.h
@@ -228,33 +228,6 @@ char *strsep(char **strp, const char *delim);
 #define MALLOC_STRUCTURE(a) ((a *)malloc(sizeof(a)))
 
 #define CHECK_AND_FREE(a) if ((a) != NULL) { free((a)); (a) = NULL;}
-#ifndef HAVE_GLIB_H
-typedef char gchar;
-typedef unsigned char guchar;
-
-typedef int gint;
-typedef unsigned int guint;
-
-typedef long glong;
-typedef unsigned long gulong;
-
-typedef double gdouble;
-
-typedef int gboolean;
-
-typedef int16_t gint16;
-typedef uint16_t guint16;
-
-typedef int32_t gint32;
-typedef uint32_t guint32;
-
-typedef int64_t gint64;
-typedef uint64_t guint64;
-
-typedef uint8_t  guint8;
-typedef int8_t gint8;
-
-#endif
 
 #ifndef TRUE
 #define TRUE 1
--- Input/aac/src/mp4_utils.c
+++ Input/aac/src/mp4_utils.c
@@ -54,7 +54,7 @@ int getAACTrack(MP4FileHandle file)
     const char *trackType = MP4GetTrackType(file, trackID);
     if(!strcmp(trackType, MP4_AUDIO_TRACK_TYPE)){//we found audio track !
       int j=0;
-      u_int8_t audiotype = MP4GetTrackAudioType(file, trackID);
+      u_int8_t audiotype = MP4GetTrackEsdsObjectTypeId(file, trackID);
       while(mp4AudioTypes[j]){ // what kind of audio is ?
 	if(mp4AudioTypes[j] == audiotype){
 	  if(mp4AudioTypes[j] == MP4_MPEG4_AUDIO_TYPE){//MPEG4 audio ok
@@ -126,7 +126,7 @@ void getMP4info(char* file)
     printf("Track %d, %s", trackID, trackType);
     if(!strcmp(trackType, MP4_AUDIO_TRACK_TYPE)){//we found audio track !
       int j=0;
-      u_int8_t audiotype = MP4GetTrackAudioType(mp4file, trackID);
+      u_int8_t audiotype = MP4GetTrackEsdsObjectTypeId(mp4file, trackID);
       while(mp4AudioTypes[j]){ // what kind of audio is ?
 	if(mp4AudioTypes[j] == audiotype){
 	  if(mp4AudioTypes[j] == MP4_MPEG4_AUDIO_TYPE){
