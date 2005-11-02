1afbee6c2dc4bc8766bd4223e17db9ef577eef2b
nenolod <devnull@localhost>
2005-11-01 20:45:32 -0800

    [svn] Some routines here. Hehe.

--- Plugins/Input/console/Audacious_Driver.cpp
+++ Plugins/Input/console/Audacious_Driver.cpp
@@ -6,4 +6,44 @@
  * http://www.slack.net/~ant/libs/
  */
 
+#include "Audacious_Driver.h"
 
+#include "libaudacious/configfile.h"
+#include "libaudacious/util.h"
+#include "libaudacious/titlestring.h"
+#include <gtk/gtk.h>
+
+#include <cstring>
+
+static Spc_Emu *spc = NULL;
+static GThread decode_thread;
+
+static int is_our_file(gchar *filename)
+{
+	gchar *ext;
+
+	ext = strrchr(filename, '.');
+
+	if (ext)
+	{
+		if (!strcasecmp(ext, ".spc"))
+			return 1;
+	}
+
+	return 0;
+}
+
+static gchar *get_title(gchar *filename)
+{
+	TitleInput *input;
+	gchar *title;
+	Emu_Std_Reader reader;
+	Spc_Emu::header_t header;
+
+	reader.open(filename);
+	reader.read(&header, sizeof(header));
+
+	title = g_strdup(header.song);
+
+	return title;
+}
---
+++ Plugins/Input/console/Audacious_Driver.h
@@ -0,0 +1,44 @@
+/*
+ * Audacious: Cross platform media player
+ * Copyright (c) 2005  Audacious team
+ *
+ * Header for the Audacious plugin.
+ */
+
+#ifndef AUDACIOUS_DRIVER_H
+#define AUDACIOUS_DRIVER_H
+
+#include "Blip_Buffer.h"
+#include "Blip_Synth.h"
+#include "Classic_Emu.h"
+#include "Effects_Buffer.h"
+#include "Fir_Resampler.h"
+#include "Gb_Apu.h"
+#include "Gb_Cpu.h"
+#include "Gb_Oscs.h"
+#include "Gbs_Emu.h"
+#include "Gym_Emu.h"
+#include "Multi_Buffer.h"
+#include "Music_Emu.h"
+#include "Nes_Apu.h"
+#include "Nes_Cpu.h"
+#include "Nes_Namco.h"
+#include "Nes_Oscs.h"
+#include "Nes_Vrc6.h"
+#include "Nsf_Emu.h"
+#include "Panning_Buffer.h"
+#include "Sms_Apu.h"
+#include "Sms_Oscs.h"
+#include "Snes_Spc.h"
+#include "Spc_Cpu.h"
+#include "Spc_Dsp.h"
+#include "Spc_Emu.h"
+#include "Tagged_Data.h"
+#include "Vgm_Emu.h"
+#include "abstract_file.h"
+#include "blargg_common.h"
+#include "blargg_endian.h"
+#include "blargg_source.h"
+#include "ym2612.h"
+
+#endif
