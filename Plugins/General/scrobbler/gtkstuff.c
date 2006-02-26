7c11b1f2d105446484de7553296889bfeb2bfd95
nenolod <devnull@localhost>
2006-02-25 19:17:26 -0800

    [svn] - Fix error box stuff.

--- Plugins/General/scrobbler/gtkstuff.c
+++ Plugins/General/scrobbler/gtkstuff.c
@@ -9,7 +9,6 @@
 #include "config.h"
 #include "md5.h"
 
-static int errorbox_done;
 void about_show(void)
 {
 	static GtkWidget *aboutbox;
@@ -28,40 +27,18 @@ void about_show(void)
 			GTK_SIGNAL_FUNC(gtk_widget_destroyed), &aboutbox);
 }
 
-static void set_errorbox_done(GtkWidget *errorbox, GtkWidget **errorboxptr)
-{
-	errorbox_done = -1;
-	gtk_widget_destroyed(errorbox, errorboxptr);
-}
-
-void init_errorbox_done(void)
-{
-	errorbox_done = 1;
-}
-
-int get_errorbox_done(void)
-{
-	return errorbox_done;
-}
-
 void errorbox_show(char *errortxt)
 {
-	static GtkWidget *errorbox;
 	gchar *tmp;
 
-	if(errorbox_done != 1)
-		return;
-	errorbox_done = 0;
 	tmp = g_strdup_printf("There has been an error"
 			" that may require your attention.\n\n"
 			"Contents of server error:\n\n"
 			"%s\n",
 			errortxt);
 
-	errorbox = xmms_show_message("Scrobbler Error",
+	xmms_show_message("Scrobbler Error",
 			tmp,
 			"OK", FALSE, NULL, NULL);
 	g_free(tmp);
-	gtk_signal_connect(GTK_OBJECT(errorbox), "destroy",
-			GTK_SIGNAL_FUNC(set_errorbox_done), &errorbox);
 }
