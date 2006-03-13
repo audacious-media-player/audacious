f327ee3f3d74a81b9c5cd122737a45f77cde4bce
nenolod <devnull@localhost>
2006-03-13 11:07:18 -0800

    [svn] - use extern here (grr)

--- libaudacious/vfs.h
+++ libaudacious/vfs.h
@@ -9,34 +9,34 @@ typedef struct _VFSFile VFSFile;
 G_BEGIN_DECLS
 
 /* Reserved for private use by BMP */
-gboolean vfs_init(void);
+extern gboolean vfs_init(void);
 
-VFSFile * vfs_fopen(const gchar * path,
+extern VFSFile * vfs_fopen(const gchar * path,
                     const gchar * mode);
-gint vfs_fclose(VFSFile * file);
+extern gint vfs_fclose(VFSFile * file);
 
-size_t vfs_fread(gpointer ptr,
+extern size_t vfs_fread(gpointer ptr,
                  size_t size,
                  size_t nmemb,
                  VFSFile * file);
-size_t vfs_fwrite(gconstpointer ptr,
+extern size_t vfs_fwrite(gconstpointer ptr,
                   size_t size,
                   size_t nmemb,
                   VFSFile *file);
 
-gint vfs_fseek(VFSFile * file,
+extern gint vfs_fseek(VFSFile * file,
                glong offset,
                gint whence);
-void vfs_rewind(VFSFile * file);
-glong vfs_ftell(VFSFile * file);
-gboolean vfs_feof(VFSFile * file);
+extern void vfs_rewind(VFSFile * file);
+extern glong vfs_ftell(VFSFile * file);
+extern gboolean vfs_feof(VFSFile * file);
 
-gboolean vfs_file_test(const gchar * path,
+extern gboolean vfs_file_test(const gchar * path,
                        GFileTest test);
 
-gboolean vfs_is_writeable(const gchar * path);
+extern gboolean vfs_is_writeable(const gchar * path);
 
-gboolean vfs_truncate(VFSFile * file, glong length);
+extern gboolean vfs_truncate(VFSFile * file, glong length);
 
 
 G_END_DECLS
