c2f4f36de5f0ef3790d08932b8333ed88420b67b
nemo <devnull@localhost>
2006-08-02 16:06:49 -0700

    [svn] fine

--- audacious/prefswin.c
+++ audacious/prefswin.c
@@ -5,7 +5,7 @@
  *  Copyright (C) 2003-2004  BMP development team.
  *
  *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public Licensse as published by
+ *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation; either version 2 of the License, or
  *  (at your option) any later version.
  *
