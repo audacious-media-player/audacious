4d9b64ca5f721fcff07c3eb6b50fde542d1d3aaa
nenolod <devnull@localhost>
2006-09-18 01:44:39 -0700

    [svn] - merge libaudacious (public) and audacious util APIs

--- audacious/util.h
+++ audacious/util.h
@@ -128,4 +128,26 @@ typedef struct {
 gchar *chardet_to_utf8(const gchar *str, gssize len,
 		       gsize *arg_bytes_read, gsize *arg_bytes_write, GError **arg_error);
 
+/* XMMS names */
+
+#define bmp_info_dialog(title, text, button_text, model, button_action, action_data) \
+  xmms_show_message(title, text, button_text, model, button_action, action_data)
+
+#define bmp_usleep(usec) \
+  xmms_usleep(usec)
+
+#define bmp_check_realtime_priority() \
+  xmms_check_realtime_priority()
+
+G_BEGIN_DECLS
+
+GtkWidget *xmms_show_message(const gchar * title, const gchar * text,
+                             const gchar * button_text, gboolean modal,
+                             GtkSignalFunc button_action,
+                             gpointer action_data);
+gboolean xmms_check_realtime_priority(void);
+void xmms_usleep(gint usec);
+
+G_END_DECLS
+
 #endif
