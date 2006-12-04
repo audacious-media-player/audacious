b68edcd597cd25a2d083c1f1f66016fe0c8db376
nenolod <devnull@localhost>
2006-12-04 01:37:47 -0800

    [svn] - document the formatter class

--- libaudacious/formatter.c
+++ libaudacious/formatter.c
@@ -1,3 +1,26 @@
+/*  Audacious
+ *  Copyright (C) 2005-2007  Audacious team
+ *
+ *  XMMS - Cross-platform multimedia player
+ *  Copyright (C) 1998-2003  Peter Alm, Mikael Alm, Olle Hallnas,
+ *                           Thomas Nilsson and 4Front Technologies
+ *  Copyright (C) 1999-2003  Haavard Kvaalen
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
 #ifdef HAVE_CONFIG_H
 #  include "config.h"
 #endif
@@ -6,7 +29,11 @@
 #include <string.h>
 #include "formatter.h"
 
-
+/**
+ * xmms_formatter_new:
+ *
+ * Factory for #Formatter objects.
+ **/
 Formatter *
 xmms_formatter_new(void)
 {
@@ -16,6 +43,12 @@ xmms_formatter_new(void)
     return formatter;
 }
 
+/**
+ * xmms_formatter_destroy:
+ * @formatter: A #Formatter object to destroy.
+ *
+ * Destroys #Formatter objects.
+ **/
 void
 xmms_formatter_destroy(Formatter * formatter)
 {
@@ -27,6 +60,14 @@ xmms_formatter_destroy(Formatter * formatter)
     g_free(formatter);
 }
 
+/**
+ * xmms_formatter_associate:
+ * @formatter: A #Formatter object to use.
+ * @id: The character to use for replacement.
+ * @value: The value to replace with.
+ *
+ * Adds a id->replacement set to the formatter's stack.
+ **/
 void
 xmms_formatter_associate(Formatter * formatter, guchar id, char *value)
 {
@@ -34,6 +75,13 @@ xmms_formatter_associate(Formatter * formatter, guchar id, char *value)
     formatter->values[id] = g_strdup(value);
 }
 
+/**
+ * xmms_formatter_dissociate:
+ * @formatter: A #Formatter object to use.
+ * @id: The id to remove the id->replacement mapping for.
+ *
+ * Removes an id->replacement mapping from the formatter's stack.
+ **/
 void
 xmms_formatter_dissociate(Formatter * formatter, guchar id)
 {
@@ -42,6 +90,15 @@ xmms_formatter_dissociate(Formatter * formatter, guchar id)
     formatter->values[id] = 0;
 }
 
+/**
+ * xmms_formatter_format:
+ * @formatter: A #Formatter object to use.
+ * @format: A string to format.
+ *
+ * Performs id->replacement substitution on a string.
+ *
+ * Returns: The formatted string.
+ **/
 gchar *
 xmms_formatter_format(Formatter * formatter, char *format)
 {
