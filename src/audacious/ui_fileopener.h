c1ac57604aa593abc4dd9e05371ac39572c3002a
mf0102 <devnull@localhost>
2007-01-26 14:55:25 -0800

    [svn] oops, forgot to add the fileopener files

---
+++ src/audacious/ui_fileopener.h
@@ -0,0 +1,28 @@
+/*  Audacious - Cross-platform multimedia player
+ *  Copyright (C) 2005-2007  Audacious development team
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; under version 2 of the License.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+#ifndef UI_FILEOPENER_H
+#define UI_FILEOPENER_H
+
+#include <gtk/gtk.h>
+
+#define NO_PLAY_BUTTON  FALSE
+#define PLAY_BUTTON     TRUE
+
+void run_filebrowser(gboolean clear_pl_on_ok);
+
+#endif
