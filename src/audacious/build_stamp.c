f823003026bcb782138d5061fdea6c926286f0a1
nenolod <devnull@localhost>
2007-02-01 11:05:54 -0800

    [svn] - add build_stamp.c so that it can be autogenerated

---
+++ src/audacious/build_stamp.c
@@ -0,0 +1,6 @@
+/* SVN repository version.
+ * This auto-generated file must not be included in SVN; but must
+ * be included in release tarballs.
+ */
+#include <glib.h>
+const gchar *svn_stamp = "";
