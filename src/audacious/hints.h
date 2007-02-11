009bb972f3d9d170cd2d65ae5e9e5ee0a9dabc5a
nenolod <devnull@localhost>
2007-02-10 20:16:31 -0800

    [svn] - bring hints.h into something we like to call reality

--- src/audacious/hints.h
+++ src/audacious/hints.h
@@ -27,17 +27,7 @@
 #include <glib.h>
 #include <gtk/gtk.h>
 
-/* Window Managers */
-#define WM_HINTS_NONE				0
-#define WM_HINTS_GNOME				1
-#define WM_HINTS_NET				2
-
-void check_wm_hints(void);
 void hint_set_always(gboolean always);
-void hint_set_skip_winlist(GtkWidget * window);
 void hint_set_sticky(gboolean sticky);
-gboolean hint_always_on_top_available(void);
-gboolean hint_move_resize_available(void);
-void hint_move_resize(GtkWidget * window, gint x, gint y, gboolean move);
 
 #endif
