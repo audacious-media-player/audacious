179adcce2942271cca082e8907b98262f5cd8447
js <devnull@localhost>
2007-03-26 09:21:40 -0700

    [svn] Don't show a warning dialog when trying to add .{jpeg,jpg,txt} files.

--- src/audacious/input.c
+++ src/audacious/input.c
@@ -290,10 +290,25 @@ input_dont_show_warning(GtkObject * object, gpointer user_data)
         !gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(object));
 }
 
+static gchar *extname(const char *filename)
+{
+    gchar *ext = strrchr(filename, '.');
+
+    if (ext != NULL) {
+        ++ext;
+        return ext;
+    }
+    else return "";
+}
 
 void
 input_show_unplayable_files(const gchar * filename)
 {
+    if (!strcmp(extname(filename), "jpeg") ||
+      !strcmp(extname(filename), "jpg") ||
+      !strcmp(extname(filename), "txt"))
+        return;
+
     static GtkWidget *dialog = NULL;
     static GtkListStore *store = NULL;
 
