c6eda534c13ed87c30dc8b10ae31ecb125c9b9a7
nenolod <devnull@localhost>
2007-05-11 21:19:21 -0700

    [svn] - add stop, pause, next, prev, play functions

--- contrib/xchat-audacious.py
+++ contrib/xchat-audacious.py
@@ -32,4 +32,26 @@ def command_np(word, word_eol, userdata):
 
 	return xchat.EAT_ALL
 
+def command_next(word, word_eol, userdata):
+	bus.get_object('org.atheme.audacious', '/org/atheme/audacious').Next()
+
+def command_prev(word, word_eol, userdata):
+	bus.get_object('org.atheme.audacious', '/org/atheme/audacious').Reverse()
+
+def command_pause(word, word_eol, userdata):
+	bus.get_object('org.atheme.audacious', '/org/atheme/audacious').Pause()
+
+def command_stop(word, word_eol, userdata):
+	bus.get_object('org.atheme.audacious', '/org/atheme/audacious').Stop()
+
+def command_play(word, word_eol, userdata):
+	bus.get_object('org.atheme.audacious', '/org/atheme/audacious').Play()
+
 xchat.hook_command("NP", command_np, help="Displays current playing song.")
+xchat.hook_command("NEXT", command_next, help="Advances in Audacious' playlist.")
+xchat.hook_command("PREV", command_prev, help="Goes backwards in Audacious' playlist.")
+xchat.hook_command("PAUSE", command_np, help="Toggles paused status.")
+xchat.hook_command("STOP", command_np, help="Stops playback.")
+xchat.hook_command("PLAY", command_np, help="Begins playback.")
+
+print "xchat-audacious $Id: xchat-audacious.py 4524 2007-05-12 04:19:21Z nenolod $ loaded"
