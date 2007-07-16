ad85804be1afbfc14add3c26ae5ff5e18f80b1c9
William Pitcock <nenolod@atheme-project.org>
2007-07-16 15:52:39 -0500

    Use playback_get_playing() for while loop.

--- src/audacious/playback.c
+++ src/audacious/playback.c
@@ -232,7 +232,7 @@ playback_control_thread(gpointer data)
 
     hook_call("playback begin", playback->entry);
 
-    while (ip_data.playing == TRUE && playback->eof == FALSE && playback->playing == TRUE)
+    while (playback_get_playing())
     {
         GTimeVal tmwait;
         g_get_current_time(&tmwait);
