db06b1968d0c2f20f4357e792deed1a29d92880d
Matti Hamalainen <ccr@tnsp.org>
2007-09-09 22:58:43 +0300

    Oops, forgot this file.

--- src/audacious/util.c
+++ src/audacious/util.c
@@ -894,7 +894,7 @@ GdkPixmap *create_dblsize_pixmap(GdkPixmap *pix) {
  * Return value: A GTK widget handle for the message box.
  **/
 GtkWidget *
-xmms_show_message(const gchar * title, const gchar * text,
+audacious_info_dialog(const gchar * title, const gchar * text,
                   const gchar * button_text, gboolean modal,
                   GtkSignalFunc button_action, gpointer action_data)
 {
