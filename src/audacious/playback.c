72149b19722818bc8ee3198b3e299c87ae079064
William Pitcock <nenolod@atheme.org>
2007-10-11 16:14:21 -0500

    playback_get_length(): Support InputPlayback::set_params().

--- src/audacious/playback.c
+++ src/audacious/playback.c
@@ -162,11 +162,15 @@ playback_get_length(void)
     g_return_val_if_fail(playback_get_playing(), -1);
     playback = get_current_input_playback();
 
+    if (playback->length)
+        return playback->length;
+
     if (playback && playback->plugin->get_song_tuple) {
         Tuple *tuple = playback->plugin->get_song_tuple(playback->filename);
         if (tuple_get_value_type(tuple, FIELD_LENGTH, NULL) == TUPLE_INT)
             return tuple_get_value_type(tuple, FIELD_LENGTH, NULL);
     }
+
     return -1;
 }
 
