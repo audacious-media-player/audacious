35c7dba8787f06f4f0902be73d3a2eeea1a1d85b
mf0102 <0102@gmx.at>
2007-10-18 14:51:10 +0200

    added notice to POTFILES.in about update-potfiles.sh, ran update-po

--- po/update-potfiles.sh
+++ po/update-potfiles.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 rm POTFILES*
+echo "# Please don't update this file manually - use ./update-potfiles.sh instead!" > POTFILES.in
 cd ..
-find src/ \( -name "*.c*" -o -name "*.glade" \) -exec grep -lE "translatable|_\(" \{\} \; | sort | uniq > po/POTFILES.in
+find src/ \( -name "*.c*" -o -name "*.glade" \) -exec grep -lE "translatable|_\(" \{\} \; | sort | uniq >> po/POTFILES.in
