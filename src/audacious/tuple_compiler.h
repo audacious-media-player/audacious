663ba595636553c5a25060442a3d90171d5bb0f3
Matti Hamalainen <ccr@tnsp.org>
2007-10-24 05:53:35 +0300

    Use UTF-8 for my name. :D

--- src/audacious/tuple_compiler.h
+++ src/audacious/tuple_compiler.h
@@ -1,6 +1,6 @@
 /*
  * Audacious - Tuplez compiler
- * Copyright (c) 2007 Matti 'ccr' H‰m‰l‰inen
+ * Copyright (c) 2007 Matti 'ccr' H√§m√§l√§inen
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
