eb0cf6b086d8e4028448de4cfda915956f26fe17
William Pitcock <nenolod@atheme.org>
2007-10-27 07:04:14 -0500

    prebuffer more data (this makes vorbis happy)

--- src/audacious/vfs_buffered_file.c
+++ src/audacious/vfs_buffered_file.c
@@ -221,7 +221,7 @@ vfs_buffered_file_new_from_uri(const gchar *uri)
 
     handle = g_new0(VFSFile, 1);
     fd = g_new0(VFSBufferedFile, 1);
-    fd->mem = g_malloc0(40000);
+    fd->mem = g_malloc0(128000);
     fd->fd = vfs_fopen(uri, "rb");
 
     if (fd->fd == NULL)
@@ -233,7 +233,7 @@ vfs_buffered_file_new_from_uri(const gchar *uri)
 	return NULL;
     }
 
-    sz = vfs_fread(fd->mem, 1, 40000, fd->fd);
+    sz = vfs_fread(fd->mem, 1, 128000, fd->fd);
     vfs_rewind(fd->fd);
 
     if (!sz)
