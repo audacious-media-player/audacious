ef577bcee2c447fb0a126da5e088f377e240f0fa
William Pitcock <nenolod@atheme.org>
2007-11-06 12:38:53 -0600

    update tuple field suggestions

--- src/audtool/audtool_handlers_vitals.c
+++ src/audtool/audtool_handlers_vitals.c
@@ -167,10 +167,10 @@ void get_current_song_tuple_field_data(gint argc, gchar **argv)
 	{
 		audtool_whine("invalid parameters for %s.", argv[0]);
 		audtool_whine("syntax: %s <fieldname>", argv[0]);
-		audtool_whine("   - fieldname example choices: performer, album_name,");
-		audtool_whine("       track_name, track_number, year, date, genre, comment,");
-		audtool_whine("       file_name, file_ext, file_path, length, formatter,");
-		audtool_whine("       custom, mtime");
+		audtool_whine("  - fieldname example choices include but are not limited to:");
+		audtool_whine("      artist, album, title, track_number, year, date,");
+		audtool_whine("      genre, comment, file_name, file_ext, file_path,");
+		audtool_whine("      length, formatter, custom, mtime");
 		exit(1);
 	}
 
