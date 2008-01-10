510f944697b86725b4896fa151665bb382bd7b71
William Pitcock <nenolod@atheme.org>
2008-01-10 07:28:34 -0600

    A better approach to the fileinfo issue.

--- src/audacious/ui_fileinfo.c
+++ src/audacious/ui_fileinfo.c
@@ -711,7 +711,7 @@ create_fileinfo_window(void)
     gtk_box_pack_start(GTK_BOX(hbox), alignment, TRUE, TRUE, 0);
 
     scrolledwindow = gtk_scrolled_window_new (NULL, NULL);
-    gtk_scrolled_window_set_policy (GTK_SCROLLED_WINDOW (scrolledwindow), GTK_POLICY_AUTOMATIC, GTK_POLICY_AUTOMATIC);
+    gtk_scrolled_window_set_policy (GTK_SCROLLED_WINDOW (scrolledwindow), GTK_POLICY_NEVER, GTK_POLICY_AUTOMATIC);
     gtk_scrolled_window_set_shadow_type (GTK_SCROLLED_WINDOW (scrolledwindow), GTK_SHADOW_IN);
     gtk_container_add(GTK_CONTAINER(arrow_rawdata), scrolledwindow);
 
