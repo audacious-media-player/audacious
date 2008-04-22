0f5d30b952fd5c3256af7c781cb35040b684439d
mf0102 <0102@gmx.at>
2008-04-22 12:14:24 +0200

    whoops, forgot to add those two files

---
+++ src/audacious/ui_playlist_evlisteners.c
@@ -0,0 +1,40 @@
+/*
+ * Audacious
+ * Copyright (c) 2006-2007 Audacious development team.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; under version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses>.
+ *
+ * The Audacious team does not consider modular code linking to
+ * Audacious or using our public API to be a derived work.
+ */
+
+#include "ui_playlist_evlisteners.h"
+
+#include <glib.h>
+
+#include "hook.h"
+#include "playlist.h"
+#include "ui_playlist.h"
+
+static void
+playlist_evlistener_playlistwin_update_list(gpointer hook_data, gpointer user_data)
+{
+    Playlist *playlist = (Playlist *) hook_data;
+
+    playlistwin_update_list(playlist);
+}
+
+void playlist_evlistener_init(void)
+{
+    hook_associate("playlistwin update list", playlist_evlistener_playlistwin_update_list, NULL);
+} 
---
+++ src/audacious/ui_playlist_evlisteners.h
@@ -0,0 +1,26 @@
+/*
+ * Audacious
+ * Copyright (c) 2006-2007 Audacious development team.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; under version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses>.
+ *
+ * The Audacious team does not consider modular code linking to
+ * Audacious or using our public API to be a derived work.
+ */
+
+#ifndef __AUDACIOUS_UI_PLAYLIST_EVLISTENERS_H__
+#define __AUDACIOUS_UI_PLAYLIST_EVLISTENERS_H__
+
+void ui_playlist_evlistener_init(void);
+
+#endif 
