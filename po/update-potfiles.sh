3001f73c1a115a7c35eea9e993d633e8cf5bbd9b
Matti Hamalainen <ccr@tnsp.org>
2008-05-21 05:45:28 +0300

    Only scan "*.{c,cxx,cc}" instead of "*.c*" when updating POTFILES.in.

--- po/update-potfiles.sh
+++ po/update-potfiles.sh
@@ -2,4 +2,4 @@
 rm POTFILES*
 echo "# Please don't update this file manually - use ./update-potfiles.sh instead!" > POTFILES.in
 cd ..
-find src/ \( -name "*.c*" -o -name "*.glade" \) -exec grep -lE "translatable|_\(" \{\} \; | sort | uniq >> po/POTFILES.in
+find src/ \( -name "*.c" -o -name "*.cxx" -o -name "*.cc" -o -name "*.glade" \) -exec grep -lE "translatable|_\(" \{\} \; | sort | uniq >> po/POTFILES.in
