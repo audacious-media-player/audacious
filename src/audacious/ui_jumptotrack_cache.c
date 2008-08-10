907d1e792406d9cf16c89270a48139133b987543
Yoshiki Yazawa <yaz@honeyplnaet.jp>
2008-08-10 12:19:28 +0900

    build fix

--- src/audacious/ui_jumptotrack_cache.c
+++ src/audacious/ui_jumptotrack_cache.c
@@ -17,6 +17,10 @@
  *  Audacious or using our public API to be a derived work.
  */
 
+#ifdef HAVE_CONFIG_H
+#  include "config.h"
+#endif
+
 #include <glib.h>
 #include <string.h>
 #include <assert.h>
