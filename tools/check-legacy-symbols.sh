27ccbdb713abb71b57c3317c9b943e30a29cfe6a
William Pitcock <nenolod@atheme.org>
2008-12-05 13:50:19 -0600

    Make this tool's output a little nicer.

--- tools/check-legacy-symbols.sh
+++ tools/check-legacy-symbols.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 
-make
+make > /dev/null
 
-echo "building symbol table of legacy code."
 rm symbols.list
 for i in src/audacious/legacy/*.o; do
     objdump -t $i | grep "F .text" | cut -b49- >> symbols.list
 done
 
 for i in `cat symbols.list`; do
-    grep $i src/audacious/*.c
+    grep -n $i src/audacious/*.c
 done
