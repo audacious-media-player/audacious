ad3eacf850972c5ace47a6d16cad2781c2b2a492
Paula Stanciu <devnull@localhost>
2009-07-11 04:13:53 +0300

    created some helper functions for file reading

--- src/libaudtag/util.c
+++ src/libaudtag/util.c
@@ -90,3 +90,74 @@ void printTuple(Tuple *tuple) {
 
     printf("-----------------END---------------------\n");
 }
+
+guint16 fread_16(VFSFile *f) {
+    guint16 i;
+    if (vfs_fread(&i, 2, 1, f) == 2) {
+        return i;
+    }
+    return -1;
+}
+
+guint32 fread_32(VFSFile *f) {
+    guint32 i;
+    if (vfs_fread(&i, 4, 1, f) == 4) {
+        return i;
+    }
+    return -1;
+}
+
+guint64 fread_64(VFSFile *f) {
+    guint64 i;
+    if (vfs_fread(&i, 8, 1, f) == 8) {
+        return i;
+    }
+    return -1;
+}
+
+/*
+ * Returns a UTF8 string converted from UTF16(as read from the file)
+ * 
+ * size is the number of bytes we should read from the file
+ * 
+ * The string may or may not be NULL-terminated
+ * 
+ * Valid string representations:
+ * "ABC" = [65 00, 66 00, 67 00,00 00], size=8 (NULL-terminated)
+ * or                           ^^^^^ 
+ * "ABC" = [65 00, 66 00, 67 00], size=6 (not NULL-terminated)
+ */
+gchar* fread_str(VFSFile *f, int size) {
+    gunichar2 *s;
+    gchar *res = NULL;
+
+    /*nothing to read*/
+    if (size == 0)
+        return NULL;
+
+    s = g_new0(gunichar2, size / 2);
+    /* if read was successful */
+    if (vfs_fread(s, size, 1, f) == size) {
+
+        /* if the string is NULL-terminated */
+        if (!(s[size - 2] || s[size - 1])) {
+                res = g_utf16_to_utf8(s, -1, NULL, NULL, NULL);
+            } else {
+            /* we have size defined in bytes, we need count of gunichar2's*/
+            res = g_utf16_to_utf8(s, size >> 1, NULL, NULL, NULL);
+        }
+    } else { /*read failed*/
+
+        g_free(s);
+    }
+    return res;
+}
+
+void seek(VFSFile *f, long pos) {
+
+    vfs_fseek(f, pos, SEEK_SET);
+}
+
+void skip(VFSFile *f, int amount) {
+    vfs_fseek(f, amount, SEEK_CUR);
+}
