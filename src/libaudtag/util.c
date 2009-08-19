ef5df5a565826bb32e6aee58777467b59f746e75
Paula Stanciu <devnull@localhost>
2009-08-19 22:38:41 +0300

    reffactored the file a bit

--- src/libaudtag/util.c
+++ src/libaudtag/util.c
@@ -1,13 +1,16 @@
 #include <glib.h>
 #include "util.h"
+#include <inttypes.h>
 
 /* convert windows time to unix time */
-time_t unix_time(guint64 win_time) {
+time_t unix_time(guint64 win_time)
+{
     guint64 t = (guint64) ((win_time / 10000000LL) - 11644473600LL);
     return (time_t) t;
 }
 
-guint16 get_year(guint64 win_time) {
+guint16 get_year(guint64 win_time)
+{
     GDate* d = g_date_new();
     g_date_set_time_t(d, unix_time(win_time));
     guint16 year = g_date_get_year(d);
@@ -16,9 +19,10 @@ guint16 get_year(guint64 win_time) {
 }
 
 Tuple *makeTuple(Tuple *tuple, const gchar* title, const gchar* artist,
-        const gchar* comment, const gchar* album,
-        const gchar * genre, const gchar* year,
-        const gchar* filePath, int tracnr) {
+                 const gchar* comment, const gchar* album,
+                 const gchar * genre, const gchar* year,
+                 const gchar* filePath, int tracnr)
+{
 
     tuple_associate_string(tuple, FIELD_ARTIST, NULL, artist);
     tuple_associate_string(tuple, FIELD_TITLE, NULL, title);
@@ -31,7 +35,8 @@ Tuple *makeTuple(Tuple *tuple, const gchar* title, const gchar* artist,
     return tuple;
 }
 
-const gchar* get_complete_filepath(Tuple *tuple) {
+const gchar* get_complete_filepath(Tuple *tuple)
+{
     const gchar* filepath;
     const gchar* dir;
     const gchar* file;
@@ -43,8 +48,8 @@ const gchar* get_complete_filepath(Tuple *tuple) {
     return filepath;
 }
 
-
-void printTuple(Tuple *tuple) {
+void print_tuple(Tuple *tuple)
+{
 #if WMA_DEBUG
     DEBUG("--------------TUPLE PRINT --------------------\n");
     const gchar* title = tuple_get_string(tuple, FIELD_TITLE, NULL);
@@ -102,119 +107,185 @@ void printTuple(Tuple *tuple) {
 #endif
 }
 
-guint16 fread_16(VFSFile *f) {
-    guint16 i;
-    if (vfs_fread(&i, 2, 1, f) == 1) {
-        return i;
-    }
-    return -1;
-}
-
-guint32 fread_32(VFSFile *f) {
-    guint32 i;
-    if (vfs_fread(&i, 4, 1, f) == 1) {
-        return i;
-    }
-    return -1;
-}
-
-guint64 fread_64(VFSFile *f) {
-    guint64 i;
-    if (vfs_fread(&i, 8, 1, f) == 8) {
-        return i;
-    }
-    return -1;
-}
-
-/*
- * Returns a UTF8 string converted from UTF16(as read from the file)
- *
- * size is the number of bytes we should read from the file
- *
- * The string may or may not be NULL-terminated
- *
- * Valid string representations:
- * "ABC" = [65 00, 66 00, 67 00,00 00], size=8 (NULL-terminated)
- * or                           ^^^^^
- * "ABC" = [65 00, 66 00, 67 00], size=6 (not NULL-terminated)
- */
-gchar* fread_str(VFSFile *f, int size) {
-    gunichar2 *s;
-    gchar *res = NULL;
-
-    /*nothing to read*/
-    if (size == 0)
-        return NULL;
-
-    s = g_new0(gunichar2, size / 2);
-    /* if read was successful */
-    if (vfs_fread(s, size, 1, f) == size) {
-
-        /* if the string is NULL-terminated */
-        if (!(s[size - 2] || s[size - 1])) {
-                res = g_utf16_to_utf8(s, -1, NULL, NULL, NULL);
-            } else {
-            /* we have size defined in bytes, we need count of gunichar2's*/
-            res = g_utf16_to_utf8(s, size >> 1, NULL, NULL, NULL);
-        }
-    } else { /*read failed*/
-
-        g_free(s);
-    }
-    return res;
-}
-
-void seek(VFSFile *f, long pos) {
+void seek(VFSFile *f, long pos)
+{
 
     vfs_fseek(f, pos, SEEK_SET);
 }
 
-void skip(VFSFile *f, int amount) {
+void skip(VFSFile *f, int amount)
+{
     vfs_fseek(f, amount, SEEK_CUR);
 }
 
-gchar *read_ASCII(VFSFile *fd, int size)
+gchar *read_char_data(VFSFile *fd, int size)
 {
-    gchar *value= g_new0(gchar,size);
-    vfs_fread(value,size,1,fd);
+    gchar *value = g_new0(gchar, size);
+    vfs_fread(value, size, 1, fd);
     return value;
 }
 
-guint32 read_int32(VFSFile *fd)
+gboolean write_char_data(VFSFile *f, gchar *data, size_t i)
 {
-    guint32 val;
-    vfs_fread(&val,4,1,fd);
-    return val;
+   return (vfs_fwrite(data, i, 1, f) == i);
+}
+gchar* utf8(gunichar2* s)
+{
+    g_return_val_if_fail(s != NULL, NULL);
+    return g_utf16_to_utf8(s, -1, NULL, NULL, NULL);
 }
 
-guint32 read_BEint32(VFSFile *fd)
+gunichar2 *fread_utf16(VFSFile* f, guint64 size)
+{
+    gunichar2 *p = (gunichar2 *) g_malloc0(size);
+    if (vfs_fread(p, 1, size, f) != size)
+    {
+        g_free(p);
+        p = NULL;
+    }
+    gchar * s = utf8(p);
+    DEBUG("Converted to UTF8: '%s'\n", s);
+    g_free(s);
+    return p;
+}
+
+gboolean write_utf16(VFSFile *f, gunichar2 *data, size_t i)
+{
+  return (vfs_fwrite(data, i, 1, f) == i);
+}
+
+guint8 read_uint8(VFSFile *fd)
+{
+    guint16 i;
+    if (vfs_fread(&i, 1, 1, fd) == 1)
+    {
+        return i;
+    }
+    return -1;
+}
+
+guint16 read_LEuint16(VFSFile *fd)
+{
+    guint16 a;
+    if (vfs_fget_le16(&a, fd))
+    {
+        DEBUG("read_LEuint16 %d\n", a);
+        return a;
+    } else
+        return -1;
+}
+
+guint16 read_BEuint16(VFSFile *fd)
+{
+    guint16 a;
+    if (vfs_fget_be16(&a, fd))
+    {
+        DEBUG("read_BEuint16 %d\n", a);
+        return a;
+    } else
+        return -1;
+}
+
+guint32 read_LEuint32(VFSFile *fd)
 {
     guint32 a;
-    vfs_fget_be32(&a,fd);
-
-    return a;
+    if (vfs_fget_le32(&a, fd))
+    {
+        DEBUG("read_LEuint32 %d\n", a);
+        return a;
+    } else
+        return -1;
 }
 
-void write_BEint32(VFSFile *fd,guint32 val)
+guint32 read_BEuint32(VFSFile *fd)
 {
-     vfs_fput_be32(val,fd);
+    guint32 a;
+    if (vfs_fget_be32(&a, fd))
+    {
+        DEBUG("read_BEuint32 %d\n", a);
+        return a;
+    } else
+        return -1;
 }
 
-
-guint64 read_int64(VFSFile *fd)
+guint64 read_LEuint64(VFSFile *fd)
 {
-    guint64 val = 0;
-    vfs_fread(&val,8,1,fd);
-    return val;
+    guint64 a;
+    if (vfs_fget_le64(&a, fd))
+    {
+        DEBUG("read_LEuint64 %"PRId64"\n", a);
+        return a;
+    } else
+        return -1;
+}
+
+guint64 read_BEuint64(VFSFile *fd)
+{
+    guint64 a;
+    if (vfs_fget_be64(&a, fd))
+    {
+        DEBUG("read_BEuint14 %"PRId64"\n", a);
+        return a;
+    } else
+        return 1;
+}
+
+gboolean write_uint8(VFSFile *fd, guint8 val)
+{
+    return (vfs_fwrite(&val, 1, 1, fd) == 1);
+}
+
+gboolean write_LEuint16(VFSFile *fd, guint16 val)
+{
+    guint16 le_val = GUINT32_TO_LE(val);
+    return (vfs_fwrite(&le_val, 2, 1, fd) == 2);
+}
+
+gboolean write_BEuint32(VFSFile *fd, guint32 val)
+{
+    guint32 be_val = GUINT32_TO_BE(val);
+    return (vfs_fwrite(&be_val, 4, 1, fd) == 4);
+}
+
+gboolean write_LEuint32(VFSFile *fd, guint32 val)
+{
+    guint32 le_val = GUINT32_TO_LE(val);
+    return (vfs_fwrite(&le_val, 4, 1, fd) == 4);
+}
+
+gboolean write_LEuint64(VFSFile *fd, guint64 val)
+{
+    guint64 le_val = GUINT64_TO_LE(val);
+    return (vfs_fwrite(&le_val, 8, 1, fd) == 8);
 }
 
 void copyAudioToFile(VFSFile *from, VFSFile *to, guint32 pos)
 {
-    vfs_fseek(from,pos,SEEK_SET);
+    vfs_fseek(from, pos, SEEK_SET);
     while (vfs_feof(from) == 0)
     {
         gchar buf[4096];
         gint n = vfs_fread(buf, 1, 4096, from);
         vfs_fwrite(buf, n, 1, to);
+    }
+}
+
+void copyAudioData(VFSFile* from, VFSFile *to, guint32 pos_from, guint32 pos_to)
+{
+    vfs_fseek(from, pos_from, SEEK_SET);
+    int bytes_read = pos_from;
+    while (bytes_read < pos_to - 4096)
+    {
+        gchar buf[4096];
+        guint32 n = vfs_fread(buf, 1, 4096, from);
+        vfs_fwrite(buf, n, 1, to);
+        bytes_read += n;
+    }
+    if (bytes_read < pos_to)
+    {
+        guint32 buf_size = pos_to - bytes_read;
+        gchar buf2[buf_size];
+        int nn = vfs_fread(buf2, 1, buf_size, from);
+        vfs_fwrite(buf2, nn, 1, to);
     }
 }
--- src/libaudtag/util.h
+++ src/libaudtag/util.h
@@ -8,6 +8,8 @@
 
 #define WMA_DEBUG 1
 
+#define BROKEN 1
+
 #if WMA_DEBUG
 #define DEBUG(...) printf("TAG %25s:\t", __func__); printf(__VA_ARGS__);
 #else
@@ -18,20 +20,40 @@ time_t unix_time(guint64 win_time);
 
 guint16 get_year(guint64 win_time);
 
-void printTuple(Tuple *tuple);
+void print_tuple(Tuple *tuple);
 
 //Tuple *makeTuple(Tuple *tuple, const gchar* title, const gchar* artist,
 //        const gchar* comment, const gchar* album,
 //        const gchar * genre, const gchar* year,
 //        const gchar* filePath, int tracnr);
 
-
+gchar *utf8(gunichar2* s);
 const gchar* get_complete_filepath(Tuple *tuple);
 
-gchar *read_ASCII(VFSFile *fd, int size);
-guint32 read_int32(VFSFile *fd);
-guint32 read_BEint32(VFSFile *fd);
-void write_BEint32(VFSFile *fd,guint32 val);
-guint64 read_int64(VFSFile *fd);
+gchar *read_char_data(VFSFile *fd, int size);
+gboolean write_char_data(VFSFile *f, gchar *data, size_t i);
+
+gunichar2 *fread_utf16(VFSFile* f, guint64 size);
+gboolean write_utf16(VFSFile *f, gunichar2 *data, size_t i);
+
+guint8 read_uint8(VFSFile *fd);
+guint16 read_LEuint16(VFSFile *fd);
+guint16 read_BEuint16(VFSFile *fd);
+guint32 read_LEuint32(VFSFile *fd);
+guint32 read_BEuint32(VFSFile *fd);
+guint64 read_LEuint64(VFSFile *fd);
+guint64 read_BEuint64(VFSFile *fd);
+
+
+gboolean write_uint8(VFSFile *fd, guint8 val);
+gboolean write_BEuint16(VFSFile *fd, guint16 val);
+gboolean write_LEuint16(VFSFile *fd, guint16 val);
+gboolean write_BEuint32(VFSFile *fd, guint32 val);
+gboolean write_LEuint32(VFSFile *fd, guint32 val);
+gboolean write_BEuint64(VFSFile *fd, guint64 val);
+gboolean write_LEuint64(VFSFile *fd, guint64 val);
+
+guint64 read_LEint64(VFSFile *fd);
 void copyAudioToFile(VFSFile *from, VFSFile *to, guint32 pos);
+void copyAudioData(VFSFile* from, VFSFile *to, guint32 pos_from, guint32 pos_to);
 #endif
