dd264793b4c3f9c5db09656b1d12ae82bf013075
John Lindgren <john.lindgren@tds.net>
2010-04-07 11:23:50 -0400

    Add copyright + GPL to a bunch of files.

--- src/libaudtag/wma/guid.h
+++ src/libaudtag/wma/guid.h
@@ -1,3 +1,23 @@
+/*
+ * Copyright 2009 Paula Stanciu
+ *
+ * This file is part of Audacious.
+ *
+ * Audacious is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, version 3 of the License.
+ *
+ * Audacious is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Audacious. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * The Audacious team does not consider modular code linking to Audacious or
+ * using our public API to be a derived work.
+ */
+
 #ifndef _GUID_H
 #define _GUID_H
 
--- src/libaudtag/wma/wma_fmt.h
+++ src/libaudtag/wma/wma_fmt.h
@@ -1,3 +1,23 @@
+/*
+ * Copyright 2009 Paula Stanciu
+ *
+ * This file is part of Audacious.
+ *
+ * Audacious is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, version 3 of the License.
+ *
+ * Audacious is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Audacious. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * The Audacious team does not consider modular code linking to Audacious or
+ * using our public API to be a derived work.
+ */
+
 #ifndef _WMA_FMT_H
 #define _WMA_FMT_H
 
@@ -56,7 +76,7 @@ typedef enum {
     DESC_LAST
 } DescrIndexes;
 
-/* 
+/*
  * this should be fine for all headers whose content is irrelevant,
  * but the size is needed so that we can skip it
  */
