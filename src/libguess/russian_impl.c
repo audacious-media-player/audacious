5b43467fed076449ece7abe85d62f0b187a8d510
William Pitcock <nenolod@atheme.org>
2010-06-17 15:10:49 -0500

    Russian: detect iso8859-2 as well.

--- src/libguess/russian_impl.c
+++ src/libguess/russian_impl.c
@@ -3,7 +3,7 @@
 #include "guess_tab.c"
 
 /* precedence order */
-#define ORDER &utf8, &cp1251, &koi8_u, &koi8_r, &cp866, &iso8859_5
+#define ORDER &utf8, &cp1251, &koi8_u, &koi8_r, &cp866, &iso8859_2, &iso8859_5
 
 /* common */
 const char *guess_ru(const char *buf, int buflen)
@@ -17,6 +17,7 @@ const char *guess_ru(const char *buf, int buflen)
     guess_dfa cp866 = DFA_INIT(guess_cp866_st, guess_cp866_ar, "CP866");
     guess_dfa koi8_u = DFA_INIT(guess_koi8_u_st, guess_koi8_u_ar, "KOI8-U");
     guess_dfa koi8_r = DFA_INIT(guess_koi8_r_st, guess_koi8_r_ar, "KOI8-R");
+    guess_dfa iso8859_2 = DFA_INIT(guess_iso8859_2_st, guess_iso8859_2_ar, "ISO-8859-2");
     guess_dfa iso8859_5 = DFA_INIT(guess_iso8859_5_st, guess_iso8859_5_ar, "ISO-8859-5");
 
     guess_dfa *top = NULL;
