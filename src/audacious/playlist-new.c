712866cfd6ee993045fecb06f20d50fbb0efe7ad
John Lindgren <john.lindgren@aol.com>
2012-04-21 10:12:08 -0400

    Use title from describe_song() as fallback instead of filename.

--- src/audacious/playlist-new.c
+++ src/audacious/playlist-new.c
@@ -1193,7 +1193,7 @@ char * playlist_entry_get_title (int playlist_num, int entry_num, bool_t fast)
     ENTER;
 
     Entry * entry = get_entry (playlist_num, entry_num, FALSE, ! fast);
-    char * title = entry ? str_ref (entry->formatted ? entry->formatted : entry->filename) : NULL;
+    char * title = entry ? str_ref (entry->formatted ? entry->formatted : entry->title) : NULL;
 
     LEAVE_RET (title);
 }
@@ -2190,7 +2190,7 @@ char * playback_entry_get_title (void)
 
     Entry * entry = get_playback_entry (FALSE, TRUE);
     char * title = entry ? str_ref (entry->formatted ? entry->formatted :
-     entry->filename) : NULL;
+     entry->title) : NULL;
 
     LEAVE_RET (title);
 }
