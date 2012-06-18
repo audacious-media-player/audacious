828c681e5f205ad1ed2622613fd45fa287e8c761
John Lindgren <john.lindgren@aol.com>
2012-06-17 20:23:03 -0400

    This was written well before 2008.

--- src/audacious/equalizer_preset.c
+++ src/audacious/equalizer_preset.c
@@ -1,6 +1,6 @@
 /*
  * equalizer_preset.c
- * Copyright 2008-2011 Eugene Zagidullin, William Pitcock, and John Lindgren
+ * Copyright 2003-2011 Eugene Zagidullin, William Pitcock, and John Lindgren
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
