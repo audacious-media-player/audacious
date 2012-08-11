95e433b1221d1beaab1d72c0c9182c66c8eb036f
John Lindgren <john.lindgren@aol.com>
2012-08-11 11:54:44 -0400

    Hide scrollbar in jump to song dialog when it is not needed.

--- src/libaudgui/ui_jumptotrack.c
+++ src/libaudgui/ui_jumptotrack.c
@@ -311,7 +311,7 @@ static void create_window (void)
     GtkWidget * scrollwin = gtk_scrolled_window_new (NULL, NULL);
     gtk_container_add(GTK_CONTAINER(scrollwin), treeview);
     gtk_scrolled_window_set_policy(GTK_SCROLLED_WINDOW(scrollwin),
-                                   GTK_POLICY_AUTOMATIC, GTK_POLICY_ALWAYS);
+                                   GTK_POLICY_AUTOMATIC, GTK_POLICY_AUTOMATIC);
     gtk_scrolled_window_set_shadow_type(GTK_SCROLLED_WINDOW(scrollwin),
                                         GTK_SHADOW_IN);
     gtk_box_pack_start(GTK_BOX(vbox), scrollwin, TRUE, TRUE, 0);
