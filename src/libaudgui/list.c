audacious-media-player/audacious

commit b5a0cbf3bc70afc54e91cb89644267731840f64b

audgui/list: Remove duplicate get_row_at_point() function.

src/libaudgui/list.c
@@ -296,30 +296,11 @@ static bool_t key_press_cb (GtkWidget * widget, GdkEventKey * event, ListModel *
     return FALSE;
 }
 
-static int get_row_at_point (GtkWidget * widget, int x, int y)
-{
-    ListModel * model = (ListModel *) gtk_tree_view_get_model ((GtkTreeView *)
-     widget);
-
-    GtkTreePath * path = NULL;
-    gtk_tree_view_get_path_at_pos ((GtkTreeView *) widget, x, y,
-     & path, NULL, NULL, NULL);
-
-    if (! path)
-        return -1;
-
-    int row = gtk_tree_path_get_indices (path)[0];
-    g_return_val_if_fail (row >= 0 && row < model->rows, -1);
-
-    gtk_tree_path_free (path);
-    return row;
-}
-
 static bool_t motion_notify_cb (GtkWidget * widget, GdkEventMotion * event, ListModel * model)
 {
     if (MODEL_HAS_CB (model, mouse_motion))
     {
-        int row = get_row_at_point (widget, event->x, event->y);
+        int row = audgui_list_row_at_point (widget, event->x, event->y);
         model->cbs->mouse_motion (model->user, event, row);
     }
 
@@ -330,7 +311,7 @@ static bool_t leave_notify_cb (GtkWidget * widget, GdkEventMotion * event, ListM
 {
     if (MODEL_HAS_CB (model, mouse_leave))
     {
-        int row = get_row_at_point (widget, event->x, event->y);
+        int row = audgui_list_row_at_point (widget, event->x, event->y);
         model->cbs->mouse_leave (model->user, event, row);
     }
 

commit c769a8ce26e901fe5c3b930df8800b07eccc878d

Fix regression caused by b5a0cbf3bc70: audgui_list_row_at_point() takes widget, not bin window, coordinates.

src/libaudgui/list.c
@@ -300,7 +300,11 @@ static bool_t motion_notify_cb (GtkWidget * widget, GdkEventMotion * event, List
 {
     if (MODEL_HAS_CB (model, mouse_motion))
     {
-        int row = audgui_list_row_at_point (widget, event->x, event->y);
+        int x, y;
+        gtk_tree_view_convert_bin_window_to_widget_coords ((GtkTreeView *)
+         widget, event->x, event->y, & x, & y);
+
+        int row = audgui_list_row_at_point (widget, x, y);
         model->cbs->mouse_motion (model->user, event, row);
     }
 
@@ -311,7 +315,11 @@ static bool_t leave_notify_cb (GtkWidget * widget, GdkEventMotion * event, ListM
 {
     if (MODEL_HAS_CB (model, mouse_leave))
     {
-        int row = audgui_list_row_at_point (widget, event->x, event->y);
+        int x, y;
+        gtk_tree_view_convert_bin_window_to_widget_coords ((GtkTreeView *)
+         widget, event->x, event->y, & x, & y);
+
+        int row = audgui_list_row_at_point (widget, x, y);
         model->cbs->mouse_leave (model->user, event, row);
     }
 