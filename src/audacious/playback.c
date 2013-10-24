267a868b962d1c4ff22a451f29cb55fb1dc8f2fe
John Lindgren <john.lindgren@aol.com>
2013-10-24 18:46:44 -0400

    Fix the corner case where "no playlist advance" and "stop after song" are both enabled.

--- src/audacious/playback.c
+++ src/audacious/playback.c
@@ -338,7 +338,9 @@ static bool_t end_cb (void * unused)
     if (get_bool (NULL, "stop_after_current_song"))
     {
         do_stop (playlist);
-        do_next (playlist);
+
+        if (! get_bool (NULL, "no_playlist_advance"))
+            do_next (playlist);
     }
     else if (repeat_a >= 0 || repeat_b >= 0)
     {
