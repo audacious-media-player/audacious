2307bb6e868e99824201063a68280bbc51b1e0aa
John Lindgren <john.lindgren@aol.com>
2013-12-07 13:15:32 -0500

    Add TinyRWLock.

--- src/libaudcore/tinylock.c
+++ src/libaudcore/tinylock.c
@@ -19,15 +19,43 @@
 
 #include "tinylock.h"
 
+#include <limits.h>
 #include <sched.h>
+
+#define WRITE_BIT (SHRT_MAX + 1)
 
 void tiny_lock (TinyLock * lock)
 {
-    while (__sync_lock_test_and_set (lock, 1))
+    while (__builtin_expect (__sync_lock_test_and_set (lock, 1), 0))
         sched_yield ();
 }
 
 void tiny_unlock (TinyLock * lock)
 {
     __sync_lock_release (lock);
+}
+
+void tiny_lock_read (TinyRWLock * lock)
+{
+    while (__builtin_expect (__sync_fetch_and_add (lock, 1) & WRITE_BIT, 0))
+    {
+        __sync_fetch_and_sub (lock, 1);
+        sched_yield ();
+    }
+}
+
+void tiny_unlock_read (TinyRWLock * lock)
+{
+    __sync_fetch_and_sub (lock, 1);
+}
+
+void tiny_lock_write (TinyRWLock * lock)
+{
+    while (! __builtin_expect (__sync_bool_compare_and_swap (lock, 0, WRITE_BIT), 1))
+        sched_yield ();
+}
+
+void tiny_unlock_write (TinyRWLock * lock)
+{
+    __sync_fetch_and_sub (lock, WRITE_BIT);
 }
