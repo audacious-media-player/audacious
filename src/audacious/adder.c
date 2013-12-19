4c76b54163e22b1cbee38c84a164286a91ada458
John Lindgren <john.lindgren@aol.com>
2013-12-18 23:15:45 -0500

    Fix infinite loop upon encountering a recursive symbolic link.

--- src/audacious/adder.c
+++ src/audacious/adder.c
@@ -288,7 +288,7 @@ static void add_folder (char * filename, PlaylistFilterFunc filter,
     while (files)
     {
         struct stat info;
-        if (stat (files->data, & info) < 0)
+        if (lstat (files->data, & info) < 0)
             goto NEXT;
 
         if (S_ISREG (info.st_mode))
