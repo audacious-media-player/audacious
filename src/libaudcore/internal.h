5749816d6d4f397dd82ac172d05c2aeeed30eee7
John Lindgren <john.lindgren@aol.com>
2014-04-16 15:08:02 -0400

    Add missed file.

---
+++ src/libaudcore/internal.h
@@ -0,0 +1,31 @@
+/*
+ * internal.h
+ * Copyright 2014 John Lindgren
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions, and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions, and the following disclaimer in the documentation
+ *    provided with the distribution.
+ *
+ * This software is provided "as is" and without any warranty, express or
+ * implied. In no event shall the authors be liable for any damages arising from
+ * the use of this software.
+ */
+
+#ifndef LIBAUDCORE_INTERNAL_H
+#define LIBAUDCORE_INTERNAL_H
+
+void chardet_init (void);
+void chardet_cleanup (void);
+
+void eq_init (void);
+void eq_cleanup (void);
+
+void history_cleanup (void);
+
+#endif
