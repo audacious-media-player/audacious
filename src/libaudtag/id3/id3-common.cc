bb4036dc1b548044b101433fb0e5a5d63dc2c8c1
John Lindgren <john.lindgren@aol.com>
2014-05-24 06:22:59 -0400

    ID3: Ignore numeric fields if atoi() returns zero.  Closes: #440.

--- src/libaudtag/id3/id3-common.cc
+++ src/libaudtag/id3/id3-common.cc
@@ -104,7 +104,10 @@ void id3_associate_int (Tuple & tuple, int field, const char * data, int size)
 {
     StringBuf text = id3_decode_text (data, size);
 
-    if (text && atoi (text) >= 0)
+    /* Ignore zeros here.  In particular, there are many ID3 tags with invalid
+     * TLEN fields floating around, and we want to let mpg123 recalculate the
+     * length in such cases. */
+    if (text && atoi (text) > 0)
     {
         AUDDBG ("Field %i = %s.\n", field, (const char *) text);
         tuple.set_int (field, atoi (text));
