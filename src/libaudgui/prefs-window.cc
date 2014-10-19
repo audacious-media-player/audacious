2053e33e970185cf65e0e20a597abbc476dd70be
Thomas Lange <thomas-lange2@gmx.de>
2014-10-19 22:07:09 +0200

    Preferences window: Never show a horizontal scrollbar. Occurred with GTK2 under Gnome 3 and German localization.

--- src/libaudgui/prefs-window.cc
+++ src/libaudgui/prefs-window.cc
@@ -718,7 +718,7 @@ static void create_prefs_window (void)
     GtkWidget * scrolledwindow = gtk_scrolled_window_new (nullptr, nullptr);
     gtk_box_pack_start ((GtkBox *) hbox, scrolledwindow, false, false, 0);
     gtk_scrolled_window_set_policy ((GtkScrolledWindow *) scrolledwindow,
-     GTK_POLICY_AUTOMATIC, GTK_POLICY_AUTOMATIC);
+     GTK_POLICY_NEVER, GTK_POLICY_AUTOMATIC);
     gtk_scrolled_window_set_shadow_type ((GtkScrolledWindow *) scrolledwindow, GTK_SHADOW_IN);
 
     category_treeview = gtk_tree_view_new ();
