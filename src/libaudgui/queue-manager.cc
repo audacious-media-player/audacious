c5d21dc7d7160ecece4c5e62a17ef9dcbb6e8aa0
Thomas Lange <thomas-lange2@gmx.de>
2015-07-29 19:02:19 +0200

    libaudgui: Remove stray semicolon.

--- src/libaudgui/queue-manager.cc
+++ src/libaudgui/queue-manager.cc
@@ -44,7 +44,7 @@ static void get_value (void * user, int row, int column, GValue * value)
     case COLUMN_ENTRY:
         g_value_set_int (value, 1 + entry);
         break;
-    case COLUMN_TITLE:;
+    case COLUMN_TITLE:
         Tuple tuple = aud_playlist_entry_get_tuple (list, entry, Playlist::Guess);
         g_value_set_string (value, tuple.get_str (Tuple::FormattedTitle));
         break;
