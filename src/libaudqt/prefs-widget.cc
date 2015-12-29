eb1892703f607f1099051fd6edbe0527ce97cda7
John Lindgren <john.lindgren@aol.com>
2015-12-29 02:55:35 -0500

    audqt: Set number of decimal places based on the step size.

--- src/libaudqt/prefs-widget.cc
+++ src/libaudqt/prefs-widget.cc
@@ -21,6 +21,7 @@
 #include "libaudqt.h"
 
 #include <assert.h>
+#include <math.h>
 
 #include <QButtonGroup>
 #include <QComboBox>
@@ -117,6 +118,8 @@ IntegerWidget::IntegerWidget (const PreferencesWidget * parent, const char * dom
     if (parent->label)
         layout->addWidget (new QLabel (translate_str (parent->label, domain)));
 
+    m_spinner->setRange ((int) m_parent->data.spin_btn.min, (int) m_parent->data.spin_btn.max);
+    m_spinner->setSingleStep ((int) m_parent->data.spin_btn.step);
     layout->addWidget (m_spinner);
 
     if (parent->data.spin_btn.right_label)
@@ -139,8 +142,6 @@ IntegerWidget::IntegerWidget (const PreferencesWidget * parent, const char * dom
 
 void IntegerWidget::update ()
 {
-    m_spinner->setRange ((int) m_parent->data.spin_btn.min, (int) m_parent->data.spin_btn.max);
-    m_spinner->setSingleStep ((int) m_parent->data.spin_btn.step);
     m_spinner->setValue (m_parent->cfg.get_int ());
 }
 
@@ -156,6 +157,12 @@ DoubleWidget::DoubleWidget (const PreferencesWidget * parent, const char * domai
     if (parent->label)
         layout->addWidget (new QLabel (translate_str (parent->label, domain)));
 
+    auto decimals_for = [] (double step)
+        { return aud::max (0, -(int) floor (log10 (step) + 0.01)); };
+
+    m_spinner->setDecimals (decimals_for (m_parent->data.spin_btn.step));
+    m_spinner->setRange (m_parent->data.spin_btn.min, m_parent->data.spin_btn.max);
+    m_spinner->setSingleStep (m_parent->data.spin_btn.step);
     layout->addWidget (m_spinner);
 
     if (parent->data.spin_btn.right_label)
@@ -174,8 +181,6 @@ DoubleWidget::DoubleWidget (const PreferencesWidget * parent, const char * domai
 
 void DoubleWidget::update ()
 {
-    m_spinner->setRange (m_parent->data.spin_btn.min, m_parent->data.spin_btn.max);
-    m_spinner->setSingleStep (m_parent->data.spin_btn.step);
     m_spinner->setValue (m_parent->cfg.get_float ());
 }
 
