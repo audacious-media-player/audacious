audacious-media-player/audacious

commit 203b41f8e249f1d193ba5101edeebb7d9756b148

Remove operator bool() from Tuple.

src/audacious/dbus-server.cc
@@ -596,30 +596,25 @@ static gboolean do_song_tuple (Obj * obj, Invoc * invoc, unsigned pos, const cha
 {
     Tuple::Field field = Tuple::field_by_name (key);
     Tuple tuple;
-    GVariant * var = nullptr;
+    GVariant * var;
 
     if (field >= 0)
         tuple = aud_playlist_entry_get_tuple (CURRENT, pos);
 
-    if (tuple)
+    switch (tuple.get_value_type (field))
     {
-        switch (tuple.get_value_type (field))
-        {
-        case Tuple::String:
-            var = g_variant_new_string (tuple.get_str (field));
-            break;
+    case Tuple::String:
+        var = g_variant_new_string (tuple.get_str (field));
+        break;
 
-        case Tuple::Int:
-            var = g_variant_new_int32 (tuple.get_int (field));
-            break;
+    case Tuple::Int:
+        var = g_variant_new_int32 (tuple.get_int (field));
+        break;
 
-        default:
-            break;
-        }
-    }
-
-    if (! var)
+    default:
         var = g_variant_new_string ("");
+        break;
+    }
 
     FINISH2 (song_tuple, g_variant_new_variant (var));
     return true;

src/libaudcore/adder.cc
@@ -132,7 +132,7 @@ static void add_file (PlaylistAddItem && item, PlaylistFilterFunc filter,
      * itself, then probe it to expand any subtunes.  The "validate" check (used
      * to skip non-audio files when adding folders) is also nested within this
      * block; note that "validate" is always false for subtunes. */
-    if (! item.tuple && ! is_subtune (item.filename))
+    if (item.tuple.valid () && ! is_subtune (item.filename))
     {
         VFSFile file;
 
@@ -300,7 +300,7 @@ static void add_generic (PlaylistAddItem && item, PlaylistFilterFunc filter,
 
     /* If the item has a valid tuple or known decoder, or it's a subtune, then
      * assume it's a playable file and skip some checks. */
-    if (item.tuple || item.decoder || is_subtune (item.filename))
+    if (item.tuple.valid () || item.decoder || is_subtune (item.filename))
         add_file (std::move (item), filter, user, result, false);
     else
     {

src/libaudcore/playback.cc
@@ -144,7 +144,7 @@ void playback_set_info (int entry, Tuple && tuple)
     if (! lock_if (in_sync))
         return;
 
-    if (tuple && tuple != pb_info.tuple)
+    if (tuple.valid () && tuple != pb_info.tuple)
     {
         pb_info.tuple = std::move (tuple);
 
@@ -300,7 +300,7 @@ static void run_playback ()
         pb_info.filename = std::move (dec.filename);
 
     // check that we have all the necessary data
-    if (! pb_info.filename || ! pb_info.tuple || ! dec.ip ||
+    if (! pb_info.filename || ! pb_info.tuple.valid () || ! dec.ip ||
      (! dec.ip->input_info.keys[InputKey::Scheme] && ! dec.file))
     {
         pb_info.error = true;
@@ -626,7 +626,9 @@ EXPORT bool InputPlugin::read_tag (const char * filename, VFSFile & file,
     /* just call read_tuple() and read_image() */
     if (tuple)
     {
-        if (! (* tuple = read_tuple (filename, file)))
+        * tuple = read_tuple (filename, file);
+
+        if (! tuple->valid ())
             return false;
         if (image && file && file.fseek (0, VFS_SEEK_SET) != 0)
             return true; /* true: tuple was read */

src/libaudcore/playlist-cache.cc
@@ -42,7 +42,7 @@ EXPORT void aud_playlist_cache_selected (int playlist)
         Tuple tuple = aud_playlist_entry_get_tuple (playlist, i, Playlist::Nothing);
         PluginHandle * decoder = aud_playlist_entry_get_decoder (playlist, i, Playlist::Nothing);
 
-        if (tuple || decoder)
+        if (tuple.valid () || decoder)
             cache.add (filename, {filename, std::move (tuple), decoder});
     }
 
@@ -60,14 +60,14 @@ void playlist_cache_load (Index<PlaylistAddItem> & items)
 
     for (auto & item : items)
     {
-        if (item.tuple && item.decoder)
+        if (item.tuple.valid () && item.decoder)
             continue;
 
         auto node = cache.lookup (item.filename);
         if (! node)
             continue;
 
-        if (! item.tuple)
+        if (! item.tuple.valid ())
             item.tuple = node->tuple.ref ();
         if (! item.decoder)
             item.decoder = node->decoder;

src/libaudcore/playlist-utils.cc
@@ -196,7 +196,7 @@ EXPORT void aud_playlist_remove_duplicates_by_scheme (int playlist, Playlist::So
         {
             Tuple current = aud_playlist_entry_get_tuple (playlist, count);
 
-            if (last && current && compare (last, current) == 0)
+            if (last.valid () && current.valid () && compare (last, current) == 0)
                 aud_playlist_entry_set_selected (playlist, count, true);
 
             last = std::move (current);

src/libaudcore/playlist.cc
@@ -225,11 +225,11 @@ void Entry::set_tuple (Tuple && new_tuple)
     if (tuple.is_set (Tuple::StartTime) && ! tuple.is_set (Tuple::AudioFile))
         return;
 
-    scanned = (bool) new_tuple;
+    scanned = new_tuple.valid ();
     failed = false;
     error = String ();
 
-    if (! new_tuple)
+    if (! scanned)
         new_tuple.set_filename (filename);
 
     length = aud::max (0, new_tuple.get_int (Tuple::Length));
@@ -617,7 +617,7 @@ static void scan_finish (ScanRequest * request)
     if (! entry->decoder)
         entry->decoder = request->decoder;
 
-    if ((! entry->scanned || entry->failed) && request->tuple)
+    if ((! entry->scanned || entry->failed) && request->tuple.valid ())
     {
         playlist->set_entry_tuple (entry, std::move (request->tuple));
         queue_update (Metadata, playlist, entry->number, 1, DelayedUpdate);

src/libaudcore/scanner.cc
@@ -45,7 +45,7 @@ ScanRequest::ScanRequest (const String & filename, int flags, Callback callback,
      * have overlapping lifecycles--each new ScanRequest is created by the
      * callback of the previous request--so the cached cuesheet persists as long
      * as consecutive playlist entries reference it. */
-    if (! this->tuple && is_cuesheet_entry (filename))
+    if (! this->tuple.valid () && is_cuesheet_entry (filename))
         cue_cache.capture (new CueCacheRef (strip_subtune (filename)));
 }
 
@@ -73,7 +73,7 @@ void ScanRequest::run ()
     if (! audio_file)
         audio_file = filename;
 
-    bool need_tuple = (flags & SCAN_TUPLE) && ! tuple;
+    bool need_tuple = (flags & SCAN_TUPLE) && ! tuple.valid ();
     bool need_image = (flags & SCAN_IMAGE);
 
     if (! decoder)

src/libaudcore/tuple.h
@@ -130,7 +130,7 @@ public:
         return * this;
     }
 
-    explicit operator bool () const
+    bool valid () const
         { return (bool) data; }
 
     bool operator== (const Tuple & b) const;

commit 1528e780825b1fc49639ea16d332c5752051dab7

Fix crash when an invalid field name is sent via D-Bus.  Closes: #817.
Regression introduced by commit 203b41f8e249.

src/audacious/dbus-server.cc
@@ -635,26 +635,29 @@ static gboolean do_song_title (Obj * obj, Invoc * invoc, unsigned pos)
 static gboolean do_song_tuple (Obj * obj, Invoc * invoc, unsigned pos, const char * key)
 {
     Tuple::Field field = Tuple::field_by_name (key);
-    Tuple tuple;
-    GVariant * var;
+    GVariant * var = nullptr;
 
     if (field >= 0)
-        tuple = CURRENT.entry_tuple (pos);
-
-    switch (tuple.get_value_type (field))
     {
-    case Tuple::String:
-        var = g_variant_new_string (tuple.get_str (field));
-        break;
+        Tuple tuple = CURRENT.entry_tuple (pos);
 
-    case Tuple::Int:
-        var = g_variant_new_int32 (tuple.get_int (field));
-        break;
+        switch (tuple.get_value_type (field))
+        {
+        case Tuple::String:
+            var = g_variant_new_string (tuple.get_str (field));
+            break;
 
-    default:
-        var = g_variant_new_string ("");
-        break;
+        case Tuple::Int:
+            var = g_variant_new_int32 (tuple.get_int (field));
+            break;
+
+        default:
+            break;
+        }
     }
+
+    if (! var)
+        var = g_variant_new_string ("");
 
     FINISH2 (song_tuple, g_variant_new_variant (var));
     return true;