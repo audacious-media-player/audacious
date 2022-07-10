efa0dfeb7bde6c425b03b11e66f5db27ca9cdba5
Guido Falsi <mad@madpilot.net>
2022-07-10 14:45:20 +0200

    Change gettext stubs to fix build without gettext.
    These changes are required to build audacious on FreeBSD when passing --disable-nls to configure.
    
    - Change _() to strip const from the string.
    
    This one is used in https://github.com/audacious-media-player/audacious/blob/73a881fb59bb27f60654b63a95d8c3de73a026ae/src/libaudgui/infowin.cc#L265
    
    It fails due to the const qualifier
    
    - Add a gettext() stub
    
    This is used in https://github.com/audacious-media-player/audacious/blob/73a881fb59bb27f60654b63a95d8c3de73a026ae/src/libaudgui/prefs-window.cc#L534
    
    Which does not look FreeBSD specific.

--- src/libaudcore/i18n.h
+++ src/libaudcore/i18n.h
@@ -34,8 +34,9 @@
 
 #else
 
-#define _(String) (String)
+#define _(String) ((char *)String)
 #define N_(String) (String)
+#define gettext(str) (str)
 #define dgettext(package, str) (str)
 #define dngettext(package, str1, str2, count) (count > 1 ? str2 : str1)
 #define ngettext(str1, str2, count) (count > 1 ? str2 : str1)
