ddcbce7f9f4b219bb09d2c2be37363d526c0de1b
John Lindgren <john@jlindgren.net>
2022-08-31 17:12:58 -0400

    audqt: Use QTextEdit for the Song Info path. Closes: #1174

--- src/libaudqt/infowin-qt.cc
+++ src/libaudqt/infowin-qt.cc
@@ -26,11 +26,9 @@
 #include <QHBoxLayout>
 #include <QImage>
 #include <QLabel>
-#include <QPainter>
-#include <QPixmap>
 #include <QPointer>
 #include <QPushButton>
-#include <QTextDocument>
+#include <QTextEdit>
 #include <QVBoxLayout>
 
 #include <libaudcore/audstrings.h>
@@ -41,60 +39,10 @@
 #include <libaudcore/probe.h>
 
 #include "info-widget.h"
-#include "libaudqt-internal.h"
 #include "libaudqt.h"
 
 namespace audqt
 {
-
-/* This class remedies some of the deficiencies of QLabel (such as lack
- * of proper wrapping).  It can be expanded and/or made more visible if
- * it turns out to be useful outside InfoWindow. */
-class TextWidget : public QWidget
-{
-public:
-    TextWidget() { m_doc.setDefaultFont(font()); }
-
-    void setText(const QString & text)
-    {
-        m_doc.setPlainText(text);
-        updateGeometry();
-    }
-
-    void setWidth(int width)
-    {
-        m_doc.setTextWidth(width);
-        updateGeometry();
-    }
-
-protected:
-    QSize sizeHint() const override
-    {
-        qreal width = m_doc.idealWidth();
-        qreal height = m_doc.size().height();
-        return QSize(ceil(width), ceil(height));
-    }
-
-    QSize minimumSizeHint() const override { return sizeHint(); }
-
-    void changeEvent(QEvent * event) override
-    {
-        if (event->type() == QEvent::FontChange)
-        {
-            m_doc.setDefaultFont(font());
-            updateGeometry();
-        }
-    }
-
-    void paintEvent(QPaintEvent * event) override
-    {
-        QPainter painter(this);
-        m_doc.drawContents(&painter);
-    }
-
-private:
-    QTextDocument m_doc;
-};
 
 class InfoWindow : public QDialog
 {
@@ -106,7 +54,7 @@ public:
 private:
     String m_filename;
     QLabel m_image;
-    TextWidget m_uri_label;
+    QTextEdit m_uri_label;
     InfoWidget m_infowidget;
     QPushButton * m_save_btn;
 
@@ -122,14 +70,10 @@ InfoWindow::InfoWindow(QWidget * parent) : QDialog(parent)
     setContentsMargins(margins.TwoPt);
 
     m_image.setAlignment(Qt::AlignCenter);
-    m_uri_label.setWidth(2 * audqt::sizes.OneInch);
-    m_uri_label.setContextMenuPolicy(Qt::CustomContextMenu);
-
-    connect(&m_uri_label, &QWidget::customContextMenuRequested,
-            [this](const QPoint & pos) {
-                show_copy_context_menu(this, m_uri_label.mapToGlobal(pos),
-                                       QString(m_filename));
-            });
+    m_uri_label.setFixedWidth(2 * audqt::sizes.OneInch);
+    m_uri_label.setFrameStyle(QFrame::NoFrame);
+    m_uri_label.setReadOnly(true);
+    m_uri_label.viewport()->setAutoFillBackground(false);
 
     auto left_vbox = make_vbox(nullptr);
     left_vbox->addWidget(&m_image);
@@ -179,14 +123,14 @@ void InfoWindow::fillInfo(Index<PlaylistAddItem> && items,
     if (items.len() == 1)
     {
         m_filename = String(items[0].filename);
-        m_uri_label.setText((QString)uri_to_display(m_filename));
+        m_uri_label.setPlainText((QString)uri_to_display(m_filename));
         displayImage(m_filename);
         m_save_btn->setText(translate_str(N_("_Save")));
     }
     else
     {
         m_filename = String();
-        m_uri_label.setText(
+        m_uri_label.setPlainText(
             translate_str(N_("%1 files selected")).arg(items.len()));
         m_image.setPixmap(
             QIcon::fromTheme("audio-x-generic").pixmap(to_native_dpi(48)));
