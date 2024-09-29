d75d5e7e5880e5901044de8b35f791e1734fa7bd
John Lindgren <john@jlindgren.net>
2024-09-29 14:30:18 -0400

    Update Windows merge.sh script

--- win32/merge.sh
+++ win32/merge.sh
@@ -11,10 +11,10 @@ cd /C/aud-win32
 for i in `find -type f` ; do
     if test -f ${srcdir}/override/$i ; then
         cp ${srcdir}/override/$i $i
-    elif test -f /C/msys64/mingw64/$i ; then
-        cp /C/msys64/mingw64/$i $i
-    elif test -f /C/msys64/mingw64/share/qt6/plugins/${i#"./bin/"} ; then
-        cp /C/msys64/mingw64/share/qt6/plugins/${i#"./bin/"} $i
+    elif test -f /C/msys64/ucrt64/$i ; then
+        cp /C/msys64/ucrt64/$i $i
+    elif test -f /C/msys64/ucrt64/share/qt6/plugins/${i#"./bin/"} ; then
+        cp /C/msys64/ucrt64/share/qt6/plugins/${i#"./bin/"} $i
     elif test -f /C/libs/$i ; then
         cp /C/libs/$i $i
     elif test -f /C/aud/$i ; then
@@ -27,7 +27,7 @@ done
 for i in `find -name *.dll` ; do strip -s $i ; done
 for i in `find -name *.exe` ; do strip -s $i ; done
 
-cd /C/msys64/mingw64/share/qt6/translations
+cd /C/msys64/ucrt64/share/qt6/translations
 mkdir -p /C/aud-win32/bin/share/qt6/translations
 for i in `find . -name '*qt_*' ! -name '*qt_help_*' -o -name '*qtbase_*'` ; do
     cp $i /C/aud-win32/bin/share/qt6/translations/$i
