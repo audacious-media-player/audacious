audacious-media-player/audacious

commit cae0d90e28518ad40c4333da2d9deb0604d97181

audcore: Use XDG cache dir to store temporary files. Closes: #1817
Since Flatpak and Snap applications use a sandboxed /tmp directory, separated
from the host /tmp directory, no other application has access to it. This is
bad for MPRIS use-cases which require read access to the cached album art file.

$ playerctl metadata mpris:artUrl
(before) file:///tmp/audacious-temp-BK0GQ3
(after)  file:///home/thomas/.cache/audacious/temp-BK0GQ3

See also: https://github.com/flathub/org.atheme.audacious/issues/61

Co-authored-by: Sabri Ünal <yakushabb@gmail.com>

src/libaudcore/util.cc
@@ -65,7 +65,7 @@ bool dir_foreach(const char * path, DirForeachFunc func, void * user)
 
 String write_temp_file(const void * data, int64_t len)
 {
-    StringBuf name = filename_build({g_get_tmp_dir(), "audacious-temp-XXXXXX"});
+    StringBuf name = filename_build({g_get_user_cache_dir(), PACKAGE, "temp-XXXXXX"});
 
     int handle = g_mkstemp(name);
     if (handle < 0)

commit 5d2143d0d5b7dd0e66e0f9a87bc86d09b3bd398d

audcore: Create cache subdir if necessary
The "audacious" subdirectory may not exist yet.

Regression from commit cae0d90e28518ad40c43.

src/libaudcore/util.cc
@@ -32,6 +32,12 @@
 #include "audstrings.h"
 #include "runtime.h"
 
+#ifdef S_IRGRP
+#define DIRMODE (S_IRWXU | S_IRGRP | S_IXGRP | S_IROTH | S_IXOTH)
+#else
+#define DIRMODE (S_IRWXU)
+#endif
+
 const char * get_home_utf8()
 {
     static std::once_flag once;
@@ -65,7 +71,14 @@ bool dir_foreach(const char * path, DirForeachFunc func, void * user)
 
 String write_temp_file(const void * data, int64_t len)
 {
-    StringBuf name = filename_build({g_get_user_cache_dir(), PACKAGE, "temp-XXXXXX"});
+    StringBuf dir = filename_build({g_get_user_cache_dir(), PACKAGE});
+    if (g_mkdir_with_parents(dir, DIRMODE) < 0)
+    {
+        AUDERR("Error creating temporary directory: %s\n", strerror(errno));
+        return String();
+    }
+
+    StringBuf name = filename_build({dir, "temp-XXXXXX"});
 
     int handle = g_mkstemp(name);
     if (handle < 0)